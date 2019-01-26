a8edbe3f7731669e3bd78aef2403dea528a6d539
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-01-26 23:41:24 +0700

    Updated Webpack configuration

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -4,9 +4,15 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 
 module.exports = {
-    entry: "./assets/js/index.js",
+    entry: "./src/main.js",
     output: {
         filename: "../dist/build.js"
+    },
+    resolve: {
+        modules: [
+            'node_modules',
+            path.resolve(__dirname, 'src/svg')
+        ]
     },
     module: {
         rules: [
@@ -23,9 +29,30 @@ module.exports = {
                 }
             },
             {
+                test: /\.pug$/,
+                oneOf: [
+                    // this applies to `<template lang="pug">` in Vue components
+                    {
+                        resourceQuery: /^\?vue/,
+                        use: ['pug-plain-loader']
+                    },
+                    // this applies to pug imports inside JavaScript
+                    {
+                        use: ['raw-loader', 'pug-plain-loader']
+                    }
+                ]
+            },
+            {
                 test: /\.js$/,
                 exclude: /(node_modules)/,
                 loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader',
+                options: {
+                    url: true,
+                }
             },
             {
                 test: /\.scss$/,
@@ -36,10 +63,26 @@ module.exports = {
                     {
                         loader: 'sass-loader',
                         options: {
-                            data: ['assets/scss/_variables.scss', 'assets/scss/_mixins.scss', 'assets/scss/_functions.scss']
+                            data:
+                            `
+                            @import 'src/scss/scss-assets/_variables.scss';
+                            @import 'src/scss/scss-assets/_mixins.scss';
+                            @import 'src/scss/scss-assets/_functions.scss';
+                            @import 'src/scss/scss-assets/_config.scss';
+                            `
                         }
                     }
+
                 ]
+            },
+            {
+                test: /\.svg$/,
+                use: [{
+                    loader: 'html-loader',
+                    options: {
+                        minimize: true
+                    }
+                }]
             }
         ]
     },
