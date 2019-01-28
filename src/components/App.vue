19d252d964552eac0e6e16941214c8746d28817f
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-01-28 13:41:56 +0700

    Added components for portfolio

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,15 +1,19 @@
 <template lang="pug">
     div#app
         Header
+        main.main
+            Portfolio
 </template>
 
 <script>
     import Header from './header/index.vue';
+    import Portfolio from './main/portfolio/index.vue';
 
     export default {
         name: 'app',
         components: {
-            Header
+            Header,
+            Portfolio
         }
     }
 </script>
---
+++ src/components/main/portfolio/index.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+    div#portfolio
+        h2.block-title Портфолио
+        p.block-description Здесь собрана часть моих работ, которыми можно повыебываться
+        div.project-cards-wrapper
+</template>
+
+<script>
+    import Vue from 'vue';
+
+    export default {
+        data() {
+            return {
+                projects: 4
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    #portfolio {
+        text-align: center;
+    }
+
+    .block-title {
+        color: $block-title-color;
+    }
+
+    .block-description {
+        color: $block-description-color;
+    }
+
+</style>
