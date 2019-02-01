07bf700940e9681a4722f99e2663c0e23ecdbe90
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-02-01 15:47:25 +0700

    Added icon to link to Github

--- src/components/main/portfolio/project-views/index.vue
+++ src/components/main/portfolio/project-views/index.vue
@@ -18,8 +18,12 @@
                 div.project-links
                     h5.project-links__title Ссылки на проекты:
                     template(v-if="projectInfo.links.checkout")
-                        volume-link(:link="projectInfo.links.checkout") Посмотреть
-                    volume-link(:link="projectInfo.links.github") Github
+                        a(class="volume-link volume-link-light" :href="projectInfo.links.checkout" target="_blank")
+                            span.volume-link__link-text Посмотреть
+                    a(class="volume-link volume-link-dark" :href="projectInfo.links.github" target="_blank")
+                        div.volume-link__icon-block
+                        span.volume-link__link-text Github
+
         div.block-overlay(
             @click="hideWindow"
         )
@@ -106,7 +110,7 @@
             @include transition(background-color 0.3s);
 
             &:hover {
-                background-color: darken($close-project-btn-bg, 20%);
+                background-color: darken($close-project-btn-bg, 15%);
             }
         }
 
@@ -248,6 +252,63 @@
             @media screen and (min-width: map-deep-get($devices, 'mobile-l') + 1px) {
                 font-size: 1.2em;
             }
+        }
+    }
+
+    $volume-link-base-class: volume-link;
+
+    .volume-link {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        align-items: center;
+        min-height: 20px;
+        padding: 10px 20px;
+        @include border-radius(5px);
+        margin-bottom: 25px;
+        text-decoration: none;
+        @include box-shadow(
+                        0 3px 1px -2px rgba(0,0,0,.2),
+                        0 2px 2px 0 rgba(0,0,0,.14),
+                        0 1px 5px 0 rgba(0,0,0,.12)
+        );
+        @include transition(background-color 0.28s);
+
+        &-light {
+            background-color: $volume-link-light-bg;
+
+            &:hover {
+                background-color: darken($volume-link-light-bg, 15%);
+            }
+
+            .#{volume-link}__link-text {
+                color: #000000;
+            }
+        }
+
+        &-dark {
+            background-color: $volume-link-dark-bg;
+
+            .#{volume-link}__icon-block {
+                /*background-image: url("../../../../svg/github-icon.svg");*/
+                background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32.58 31.77'%3e %3cdefs%3e %3cstyle%3epath %7bfill:white%3bfill-rule:evenodd%3b%7d%3c/style%3e %3c/defs%3e %3cpath d='M152.61%2c107.44a16.29%2c16.29%2c0%2c0%2c0-5.15%2c31.75c.81.15%2c1.11-.35%2c1.11-.79s0-1.41%2c0-2.77c-4.53%2c1-5.49-2.18-5.49-2.18a4.31%2c4.31%2c0%2c0%2c0-1.81-2.38c-1.48-1%2c.11-1%2c.11-1a3.42%2c3.42%2c0%2c0%2c1%2c2.5%2c1.68%2c3.47%2c3.47%2c0%2c0%2c0%2c4.74%2c1.35%2c3.48%2c3.48%2c0%2c0%2c1%2c1-2.18c-3.62-.41-7.42-1.81-7.42-8.05a6.3%2c6.3%2c0%2c0%2c1%2c1.68-4.37%2c5.86%2c5.86%2c0%2c0%2c1%2c.16-4.31s1.37-.44%2c4.48%2c1.67a15.44%2c15.44%2c0%2c0%2c1%2c8.16%2c0c3.11-2.11%2c4.48-1.67%2c4.48-1.67a5.85%2c5.85%2c0%2c0%2c1%2c.16%2c4.31%2c6.29%2c6.29%2c0%2c0%2c1%2c1.67%2c4.37c0%2c6.26-3.81%2c7.63-7.44%2c8a3.89%2c3.89%2c0%2c0%2c1%2c1.11%2c3c0%2c2.18%2c0%2c3.93%2c0%2c4.47s.29.94%2c1.12.78a16.29%2c16.29%2c0%2c0%2c0-5.16-31.74Z' transform='translate(-136.32 -107.44)'/%3e %3c/svg%3e");
+                background-repeat: no-repeat;
+                background-size: contain;
+            }
+
+            &:hover {
+                background-color: lighten($volume-link-dark-bg, 15%);
+            }
+
+            .#{volume-link}__link-text {
+                color: #ffffff;
+            }
+        }
+
+        &__icon-block {
+            width: 20px;
+            height: 20px;
+            margin-right: 10px;
         }
     }
 
