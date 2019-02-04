13ab9e8e1d545128b92f8ff433b3a1b6966b8743
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-02-04 16:19:10 +0700

    Applied styles to cell-list
    Styles for mobile devices were added to the list.

--- src/components/main/cv/items/cell-list/index.vue
+++ src/components/main/cv/items/cell-list/index.vue
@@ -1,29 +1,63 @@
 <template lang="pug">
-    ul.cell-list
-        li.cell-list__item(v-for="item in data")
-            span.list-item-text {{ item }}
+    div.cell-list-wrapper
+        ul.cell-list(:style="{width: getWidth() + 'px'}")
+            li.cell-list__item(v-for="item in data")
+                span.list-item-text {{ item }}
 </template>
 
 <script>
     export default {
-        props: ['data']
+        props: ['data'],
+        methods: {
+            getWidth() {
+                return window.clientWidth;
+            }
+        }
     }
 </script>
 
 <style  lang="scss" scoped>
 
+    .cell-list-wrapper {
+        position: relative;
+        top: 0;
+        left: 0;
+        height: 40px;
+    }
+
     .cell-list {
+        position: absolute;
+        /*z-index: 1;*/
+        top: 0;
+        left: -15px;
         display: flex;
         flex-direction: row;
+        flex-wrap: nowrap;
+        box-sizing: border-box;
+        width: 100vw;
+        @include reset-pad-marg();
+        padding-left: 10px;
+        padding-bottom: 5px;
+        list-style-type: none;
+        overflow-x: scroll;
 
-        &__list-item {
+        &__item {
             display: flex;
-            flex-direction: column;
+            flex-direction: row;
             align-self: center;
-            padding: 15px 10px;
+            padding: 8px 15px;
             @include border-radius(5px);
             margin-right: 15px;
-            background-color: pink;
+            white-space: nowrap;
+            /*margin-bottom: 10px;*/
+            background-color: $cell-list-item-bg;
+        }
+
+        &::after {
+            content: '';
+            display: block;
+            width: 20px;
+            height: 20px;
         }
     }
 
