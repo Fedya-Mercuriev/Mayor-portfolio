79cb9d960c6982851fb81100c52a79f0ebde52ac
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-02-04 16:18:14 +0700

    Added new variables and changed default values

--- src/components/main/cv/items/cell-list/index.vue
+++ src/components/main/cv/items/cell-list/index.vue
@@ -1,13 +1,35 @@
 <template lang="pug">
-    
+    ul.cell-list
+        li.cell-list__item(v-for="item in data")
+            span.list-item-text {{ item }}
 </template>
 
 <script>
     export default {
-
+        props: ['data']
     }
 </script>
 
 <style  lang="scss" scoped>
+
+    .cell-list {
+        display: flex;
+        flex-direction: row;
+
+        &__list-item {
+            display: flex;
+            flex-direction: column;
+            align-self: center;
+            padding: 15px 10px;
+            @include border-radius(5px);
+            margin-right: 15px;
+            background-color: pink;
+        }
+    }
+
+    .list-item-text {
+        display: inline-block;
+        vertical-align: middle;
+    }
 
 </style>
