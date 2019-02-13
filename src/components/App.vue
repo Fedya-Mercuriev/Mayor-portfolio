19bb2259d862cc71e9810f6dfae097db3b5568d6
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-02-13 18:02:25 +0700

    Added section detector
    This function will be used to replace hash in order to scroll to a
    desired section.

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,18 +2,53 @@
     div#app(ref="app")
         Header
         main.main
+            Design
             Portfolio
+            CV
+            Contacts
 </template>
 
 <script>
     import Header from './header/index.vue';
+    import Design from './main/design/index.vue';
     import Portfolio from './main/portfolio/index.vue';
+    import CV from './main/cv/index.vue';
+    import Contacts from './main/contacts/index.vue';
 
     export default {
         name: 'app',
         components: {
             Header,
-            Portfolio
+            Design,
+            Portfolio,
+            CV,
+            Contacts
+        },
+        data() {
+            return {
+                currentSection: ""
+            }
+        },
+        mounted() {
+            window.scrollBy(0,0);
+            let ctx = this,
+                hash = null,
+                Yoffset = null,
+                result = null;
+            window.addEventListener('scroll', function() {
+                setTimeout(() => {
+                    Yoffset = window.pageYOffset + 60;
+                    ctx.$children.forEach(item => {
+                        if (Yoffset > item.$el.offsetTop - 1 && Yoffset < item.$el.offsetTop + item.$el.clientHeight + 1) {
+                            if (result !== item.$el.id) {
+                                result = item.$el.id;
+                                console.log(`Текущий элемент: ${result}`);
+                                return result;
+                            }
+                        }
+                    });
+                }, 200);
+            })
         },
         methods: {
             controlScroll(block) {
---
+++ src/components/main/design/index.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+    section#design
+    h2 Тут будет статья про дизайн китайских сайтов
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
