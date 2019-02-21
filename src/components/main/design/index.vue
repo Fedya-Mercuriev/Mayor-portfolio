3cfbab9cc9107133a5d94a4972f4efeb157a6214
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-02-21 14:27:35 +0700

    Saving changes for design section

--- src/components/main/design/index.vue
+++ src/components/main/design/index.vue
@@ -1,14 +1,28 @@
 <template lang="pug">
     section#design
-    h2 Тут будет статья про дизайн китайских сайтов
+        H2(:appearance="appearance") {{ blockTitle }}
+        BlockDescription(:appearance="appearance") {{ blockDescription }}
 </template>
 
 <script>
+    import H2 from 'Components/public/text-components/titles/level-2.vue';
+    import BlockDescription from 'Components/public/text-components/block-description.vue';
     export default {
-
+        components: {
+            H2,
+            BlockDescription
+        },
+        props: {
+            appearance: String
+        },
+        data() {
+            return {
+                blockTitle: "Дизайн китайских сайтов",
+                blockDescription: "Маленькое описание для раздела про дизайн китайских сайтов"
+            }
+        }
     }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
 </style>
