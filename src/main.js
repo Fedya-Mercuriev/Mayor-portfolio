940cc5c2a003dd96c5f476337aebb2d2c478ce36
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-03-01 17:44:18 +0700

    Saving chages for main js file

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import App from './components/App.vue';
 import './scss/general.scss';
+import { i18n } from './plugins/i18n.js';
+
+Vue.config.productionTip = false;
 
 let app = new Vue({
-    el: "#app",
+    // el: ,
+    i18n,
     render: h => h(App)
-});
+}).$mount('#app');
