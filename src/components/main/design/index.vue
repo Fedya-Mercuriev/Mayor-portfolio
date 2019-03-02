c756361b629b929b85a880ce616c12096f9bae1b
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-03-02 12:23:12 +0700

    Added dummy-block to design section

--- src/components/main/design/index.vue
+++ src/components/main/design/index.vue
@@ -2,15 +2,24 @@
     section#design
         H2(:appearance="appearance") {{ $t(blockTitle) }}
         BlockDescription(:appearance="appearance") {{ $t(blockDescription) }}
+        template(v-if="!blockText")
+            div.block-dummy
+                div.block-dummy__icon-wrapper
+                    NoteIcon
+                p.block-dummy__text {{ `${$t(blockDummyText)}...` }}
+
 </template>
 
 <script>
     import H2 from 'Components/public/text-components/titles/level-2.vue';
     import BlockDescription from 'Components/public/text-components/block-description.vue';
+    import NoteIcon from 'Root/svg/note-icon.svg';
+
     export default {
         components: {
             H2,
-            BlockDescription
+            BlockDescription,
+            NoteIcon
         },
         props: {
             appearance: String
@@ -18,13 +27,16 @@
         data() {
             return {
                 blockTitle: 'design.title',
-                blockDescription: 'design.description'
+                blockDescription: 'design.description',
+                blockText: "",
+                blockDummyText: 'design.blockDummyText'
             }
         }
     }
 </script>
 
 <style lang="scss" scoped>
+
     #design {
         display: flex;
         flex-direction: column;
@@ -34,5 +46,26 @@
         @media screen and (min-width: map-deep-get($devices, 'desktop') + 1px) {
             margin-bottom: 60px;
         }
+    }
+
+    .block-dummy {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        align-items: center;
+
+        &__icon-wrapper {
+            width: 40px;
+            height: 40px;
+            margin-right: 20px;
+        }
+
+        &__text {
+            color: $primary-text-color;
+        }
+    }
+
+    .note-icon {
+        fill: #bbbbbb;
     }
 </style>
